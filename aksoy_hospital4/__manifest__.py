b25bd7f5de7ea5ff61f0334753d8841433ef967e
Onur Aksoy <onur.aksoy.3445@gmail.com>
2023-09-21 23:39:40 +0300

    Test(1)

---
+++ aksoy_hospital4/__manifest__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hastane Yonetimi',
+    'version': '1.0.0',
+    'summary': 'Basit Hastane Yonetimi',
+    'sequence': -115,
+    'description': """Basit Hastane Yonetimi""",
+    'category': 'Hastane',
+    'author': 'Onur Aksoy',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale',
+        'mail',
+        'website_slides',
+        'hr',
+        'product',
+        'stock',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+
+        'wizard/create_appointment_view.xml',
+        'wizard/search_appointment_view.xml',
+
+        'views/patient_view.xml',
+        'views/doctor_view.xml',
+        'views/appointment_view.xml',
+        'views/kids_view.xml',
+        'views/patient_gender_view.xml',
+        'views/sale.xml',
+
+        'report/patient_details_template.xml',
+        'report/patient_card.xml',
+        'report/report.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ aksoy_hospital4/wizard/search_appointment.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from odoo import api, fields, models, _
+
+
+class SearchAppointmentWizard(models.TransientModel):
+    _name = "search.appointment.wizard"
+    _description = "Randevu Arama Sihirbazi"
+
+    patient_id = fields.Many2one('hospital.patient', string="Hasta", required=True)
+
+    def action_search_appointment_m1(self):
+        action = self.env.ref('aksoy_hospital4.action_hospital_appointment').read()[0]
+        action['domain'] = [('patient_id', '=', self.patient_id.id)]
+        return action
+
+    def action_search_appointment_m2(self):
+        action = self.env['ir.actions.actions']._for_xml_id("aksoy_hospital4.action_hospital_appointment")
+        action['domain'] = [('patient_id', '=', self.patient_id.id)]
+        return action
+
+    def action_search_appointment_m3(self):
+        return {
+            'type': 'ir.actions.act_window',
+            'name': 'Randevular',
+            'res_model': 'hospital.appointment',
+            'view_type': 'form',
+            'domain': [('patient_id', '=', self.patient_id.id)],
+            'view_mode': 'tree,form',
+            'target': 'current',
+        }
+
+
+
